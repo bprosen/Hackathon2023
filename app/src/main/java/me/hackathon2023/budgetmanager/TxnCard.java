b24f6d411ef9f38c24b1499a266a25997ae988cb
Shiv <shivbhagat@icloud.com>
2023-02-25 15:43:53 -0600

    added txn card for dashboard

---
+++ app/src/main/java/me/hackathon2023/budgetmanager/TxnCard.java
@@ -0,0 +1,14 @@
+package me.hackathon2023.budgetmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TxnCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_txn_card);
+    }
+}
