9b70e20e733925967bc0f82bf2594f8736dd13a3
Shiv <shivbhagat@icloud.com>
2023-02-25 15:01:25 -0600

    added dashboard && add txn page and fixed email stuff

---
+++ app/src/main/java/me/hackathon2023/budgetmanager/MoneyMatters.java
@@ -0,0 +1,14 @@
+package me.hackathon2023.budgetmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoneyMatters extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_money_matters);
+    }
+}
