9a52150374803e1e8de91cb26419ef445bb0a3e4
vanshk <vansh1604patel@gmail.com>
2023-02-25 11:53:38 -0600

    WelcomePageDone

--- app/src/main/java/me/hackathon2023/budgetmanager/LoginScreen.java
+++ app/src/main/java/me/hackathon2023/budgetmanager/LoginScreen.java
@@ -2,18 +2,29 @@ package me.hackathon2023.budgetmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.example.myapplication.R;
 
 public class LoginScreen extends AppCompatActivity {
-    private Button pushMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(LoginScreen.this, BudgetManager.class);
+                        startActivity(i);
+
+                finish();
+            }
+        }, 3000);
+
     }
 }
