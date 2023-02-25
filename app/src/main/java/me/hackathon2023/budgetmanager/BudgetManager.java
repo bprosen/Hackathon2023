d6fdce0dda25f27234b742dc30578f668895222f
xxBennny <benpro100@gmail.com>
2023-02-25 11:16:09 -0600

    fix

--- app/src/main/java/me/hackathon2023/budgetmanager/BudgetManager.java
+++ app/src/main/java/me/hackathon2023/budgetmanager/BudgetManager.java
@@ -6,8 +6,15 @@ import me.hackathon2023.budgetmanager.database.DatabaseManager;
 
 public class BudgetManager extends Activity
 {
+    private static DatabaseManager databaseManager;
+
     public static void main(String[] args)
     {
-        DatabaseManager databaseManager = new DatabaseManager();
+        databaseManager = new DatabaseManager();
+    }
+
+    public static DatabaseManager getDatabaseManager()
+    {
+        return databaseManager;
     }
 }
