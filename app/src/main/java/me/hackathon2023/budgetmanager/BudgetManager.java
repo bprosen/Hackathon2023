dc0521ec34c0e4d97a3769f3de4d96488a67268c
xxBennny <benpro100@gmail.com>
2023-02-25 14:55:11 -0600

    fix main

--- app/src/main/java/me/hackathon2023/budgetmanager/BudgetManager.java
+++ app/src/main/java/me/hackathon2023/budgetmanager/BudgetManager.java
@@ -5,20 +5,25 @@ import android.content.ContextWrapper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import me.hackathon2023.budgetmanager.data.User;
 import me.hackathon2023.budgetmanager.database.DatabaseManager;
 
 public class BudgetManager extends AppCompatActivity
 {
     private static DatabaseManager databaseManager;
+    private static User loggedInUser;
 
-    public static void main(String[] args)
+    public static void setUser(User user)
     {
+        loggedInUser = user;
     }
 
     public static void setDatabaseManager(ContextWrapper wrapper)
     {
         databaseManager = new DatabaseManager(wrapper);
     }
+
+    public static User getLoggedInUser() { return loggedInUser; }
 
     public static DatabaseManager getDatabaseManager()
     {
