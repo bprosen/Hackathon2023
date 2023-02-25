3b6566072c451fb4a3f136502b0ab1e61a5eee78
vanshk <vansh1604patel@gmail.com>
2023-02-25 11:14:17 -0600

    WelcomePage

--- app/src/main/java/me/hackathon2023/budgetmanager/BudgetManager.java
+++ app/src/main/java/me/hackathon2023/budgetmanager/BudgetManager.java
@@ -2,20 +2,12 @@ package me.hackathon2023.budgetmanager;
 
 import android.app.Activity;
 
-import me.hackathon2023.budgetmanager.data.User;
 import me.hackathon2023.budgetmanager.database.DatabaseManager;
 
 public class BudgetManager extends Activity
 {
-    private static DatabaseManager databaseManager;
-
     public static void main(String[] args)
     {
-        databaseManager = new DatabaseManager();
-    }
-
-    public static DatabaseManager getDatabaseManager()
-    {
-        return databaseManager;
+        DatabaseManager databaseManager = new DatabaseManager();
     }
 }
---
+++ app/src/main/java/me/hackathon2023/budgetmanager/LoginScreen.java
@@ -0,0 +1,22 @@
+package me.hackathon2023.budgetmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.example.myapplication.R;
+
+public class LoginScreen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+
+
+
+
+    }
+}
